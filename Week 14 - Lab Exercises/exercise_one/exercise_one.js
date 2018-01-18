022e5954abb113a27acbed854036616d2dec5697
Piotr Zadka <piotr.zadka@gmail.com>
2018-01-18 19:40:08 +0000

    Week 14

--- Week 14 - Lab Exercises/exercise_one/exercise_one.js
+++ Week 14 - Lab Exercises/exercise_one/exercise_one.js
@@ -3,29 +3,61 @@
 
 var fpsInterval;
 var then;
-
-function startAnimating(fps) {
-
-	fpsInterval = 1000/fps;
-	then = Date.now();
-	animate();
-
-}
-
 var i = 0;
 var j = 0;
 var cells = 10;
 
+function startAnimating(fps){
+	fpsInterval = 1000/fps;
+	then = Date.now();
+	animate();
+}
 
+function controlls() {
+	movingBlock();
+	wallDetection();
+}
+
+function movingBlock(){
+		if(event.charCode == "100"){
+			i = i + 1;
+		}
+		if(event.charCode == "97"){
+			i = i - 1;
+		}
+
+	if(event.charCode == "119"){
+		j = j - 1;
+	}
+	if(event.charCode == "115"){
+		j = j + 1;
+	}
+}
+
+function wallDetection(){
+	console.log("i:"+i);
+	console.log("j:"+j);
+	if(i>9){
+		i = 9;
+	}
+	if(i<0){
+		i = 0;
+	}
+	if(j>9){
+		j = 9;
+	}
+	if(j<0){
+		j = 0;
+	}
+}
 
 function animate() {
-
 	requestAnimationFrame(animate);
-
 	var now = Date.now();
 	var elapsed = now - then;
 
-	if (elapsed > fpsInterval) {
+	if (elapsed > fpsInterval)
+	{
 		then = now - (elapsed % fpsInterval);
 
 		var cvs = $("canvas").get(0);
@@ -39,30 +71,14 @@ function animate() {
 						j*cvs.height/cells,
 						cvs.width/cells,
 						cvs.height/cells);
-
-
+	}
 }
 
 $(document).ready(function(){
-
-	// (1) What sort of event handler do we need? What can we attach it to?
+// (1) What sort of event handler do we need? What can we attach it to?
 	$( document ).keypress(function(event) {
   console.log("Keypressed: "+event.charCode);
-
-	if(event.charCode == "100"){
-		i = i + 1;
-	}
-	if(event.charCode == "97"){
-		i = i - 1;
-	}
-	if(event.charCode == "119"){
-		j = j - 1;
-	}
-	if(event.charCode == "115"){
-		j = j + 1;
-	}
-
-	});
+	controlls();
+});
 	startAnimating(30);
-
 });
--- Week 14 - Lab Exercises/exercise_two/exercise_two.js
+++ Week 14 - Lab Exercises/exercise_two/exercise_two.js
@@ -1,45 +1,90 @@
-
-// (1) We will need a (global) array - how do we declare one? What should it be called?
-
 var fpsInterval;
 var then;
-		
+// store mouse X,Y position for green circle
+var mouseXY = [];
+// spawn red circle outside canvas
+var circle = {
+	x: -30,
+	y: -30
+};
+
+// keep refreshing canvas
 function startAnimating(fps)
 {
 	fpsInterval = 1000/fps;
 	then = Date.now();
 	animate();
 }
-		
-function animate() {
-		
-	requestAnimationFrame(animate);
-	
-	var now = Date.now();
-	var elapsed = now - then;
-	
-	if (elapsed > fpsInterval) {
-		then = now - (elapsed % fpsInterval);
-		
-		var cvs = $("canvas").get(0);
-		var ctx = cvs.getContext("2d");
-		ctx.clearRect(0, 0, cvs.width, cvs.height);
 
-		// (4) What do we need to draw?
-		
-	}
-	
-				
+// draw green circle
+function drawGreenCircle(x,y){
+	var cvs = $("canvas").get(0);
+	var ctx = cvs.getContext("2d");
+	ctx.beginPath();
+	ctx.arc(x,y,30,0,2*Math.PI);
+	ctx.fillStyle = 'green';
+	ctx.fill();
+	ctx.stroke();
 }
 
+// record mouse X,Y position when mouse is clicked on canvas
+function mouseClick(){
+	$( "canvas").click(function(event){
+		var mouseX = event.pageX;
+		var mouseY = event.pageY;
+
+		// add new mouse position to array
+		mouseXY.push({
+			x: mouseX,
+			y: mouseY
+		});
+
+		// if array is bigger than 5 remove first element
+		if(mouseXY.length > 5){
+			mouseXY.shift();
+		}
+	});
+}
+
+// draw red circle
+function moveRedCircle(x,y){
+	var cvs = $("canvas").get(0);
+	var ctx = cvs.getContext("2d");
+	ctx.clearRect(0, 0, cvs.width, cvs.height);
+	ctx.beginPath();
+	ctx.arc(x,y,30,0,2*Math.PI);
+	ctx.fillStyle = 'red';
+	ctx.fill();
+	ctx.stroke();
+}
+
+// keep track on mouse x,y position on canvas
+function mouseMove(){
+	$( "canvas" ).mousemove(function(event){
+		circle.x = event.pageX;
+		circle.y = event.pageY;
+	});
+}
+
+//
+function animate() {
+	requestAnimationFrame(animate);
+	var now = Date.now();
+	var elapsed = now - then;
+	if (elapsed > fpsInterval) {
+		then = now - (elapsed % fpsInterval);
+		// draw red circle on current mouse X,Y position
+		moveRedCircle(circle.x,circle.y);
+		// keep drawing green circle as long as there is new X,Y value in array
+		for(i = 0; i < mouseXY.length; i++){
+			drawGreenCircle(mouseXY[i].x, mouseXY[i].y);
+		}
+	}
+}
+
+// RUN
 $(document).ready(function(){
-
-	// (2) What sort of events do we need? What do we need to do in each different event?
-
-			
-	// (3) What do we need to do when the event is triggered?
-		
-		
+	mouseClick();
+	mouseMove();
 	startAnimating(30);
-		
 });
